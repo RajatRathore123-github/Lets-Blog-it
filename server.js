6ae1c44bd3fcdc9cc692ffae4050e924c14eb230
Rajat Rathore <rajat12350iam@gmail.com>
2023-06-04 20:17:05 +0530

    changes to server

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import connection from "./db.js"
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 import cors from "cors";
 import bodyParser from "body-parser";
@@ -13,9 +16,12 @@ app.use(bodyParser.json({ extended: true}));
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use("/",Router);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 app.use(express.static(path.join(__dirname,"client/build")));
 app.get("/*", function(req,res){
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname ,'client/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 8000;
